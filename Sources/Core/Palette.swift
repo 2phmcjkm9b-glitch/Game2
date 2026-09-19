4acecbb3d1d1ff3d78f4a8a5e577bd43d5d740cc
2phmcjkm9b-glitch <2phmcjkm9b@privaterelay.appleid.com>
2026-09-20 02:46:00 +0300

    Part 1: School13 project skeleton

---
+++ Sources/Core/Palette.swift
@@ -0,0 +1,13 @@
+import SpriteKit
+
+enum Palette {
+    static let bg = SKColor(red: 0.04, green: 0.05, blue: 0.08, alpha: 1)
+    static let bgDeep = SKColor(red: 0.02, green: 0.02, blue: 0.04, alpha: 1)
+    static let cyan = SKColor(red: 0.25, green: 0.95, blue: 1.00, alpha: 1)
+    static let magenta = SKColor(red: 1.00, green: 0.25, blue: 0.70, alpha: 1)
+    static let amber = SKColor(red: 1.00, green: 0.75, blue: 0.30, alpha: 1)
+    static let blood = SKColor(red: 0.90, green: 0.15, blue: 0.20, alpha: 1)
+    static let text = SKColor(white: 0.92, alpha: 1)
+    static let textDim = SKColor(white: 0.60, alpha: 1)
+    static let candle = SKColor(red: 1.00, green: 0.60, blue: 0.25, alpha: 1)
+}
