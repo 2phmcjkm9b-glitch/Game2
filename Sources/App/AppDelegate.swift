cc1561ad22a6de1140d8e06b582385c108191f61
2phmcjkm9b-glitch <2phmcjkm9b@privaterelay.appleid.com>
2026-09-20 03:08:06 +0300

    Fix launch crash by deferring audio engine startup

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -4,7 +4,6 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Audio.shared.start()
         return true
     }
 
