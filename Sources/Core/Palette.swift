528c8a66bf71ab85f618f3b57bfdb69b08e65b88
2phmcjkm9b-glitch <2phmcjkm9b@privaterelay.appleid.com>
2026-09-20 12:34:37 +0300

    Redesign visual palette

--- Sources/Core/Palette.swift
+++ Sources/Core/Palette.swift
@@ -1,13 +1,18 @@
 import SpriteKit
 
 enum Palette {
-    static let bg = SKColor(red: 0.04, green: 0.05, blue: 0.08, alpha: 1)
-    static let bgDeep = SKColor(red: 0.02, green: 0.02, blue: 0.04, alpha: 1)
-    static let cyan = SKColor(red: 0.25, green: 0.95, blue: 1.00, alpha: 1)
-    static let magenta = SKColor(red: 1.00, green: 0.25, blue: 0.70, alpha: 1)
-    static let amber = SKColor(red: 1.00, green: 0.75, blue: 0.30, alpha: 1)
-    static let blood = SKColor(red: 0.90, green: 0.15, blue: 0.20, alpha: 1)
-    static let text = SKColor(white: 0.92, alpha: 1)
-    static let textDim = SKColor(white: 0.60, alpha: 1)
-    static let candle = SKColor(red: 1.00, green: 0.60, blue: 0.25, alpha: 1)
+    static let bg = SKColor(red: 0.018, green: 0.022, blue: 0.035, alpha: 1)
+    static let bgDeep = SKColor(red: 0.006, green: 0.008, blue: 0.015, alpha: 1)
+    static let panel = SKColor(red: 0.045, green: 0.050, blue: 0.075, alpha: 0.94)
+    static let panelSoft = SKColor(red: 0.075, green: 0.080, blue: 0.11, alpha: 0.82)
+    static let cyan = SKColor(red: 0.22, green: 0.88, blue: 1.00, alpha: 1)
+    static let cyanSoft = SKColor(red: 0.18, green: 0.60, blue: 0.72, alpha: 1)
+    static let magenta = SKColor(red: 1.00, green: 0.16, blue: 0.62, alpha: 1)
+    static let purple = SKColor(red: 0.56, green: 0.30, blue: 1.00, alpha: 1)
+    static let amber = SKColor(red: 1.00, green: 0.67, blue: 0.20, alpha: 1)
+    static let blood = SKColor(red: 0.92, green: 0.10, blue: 0.18, alpha: 1)
+    static let text = SKColor(white: 0.96, alpha: 1)
+    static let textDim = SKColor(white: 0.58, alpha: 1)
+    static let textFaint = SKColor(white: 0.34, alpha: 1)
+    static let candle = SKColor(red: 1.00, green: 0.52, blue: 0.16, alpha: 1)
 }
